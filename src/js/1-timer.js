7dcdd3e13e969f42cd430a939d33b93fc7b24eaa
Vadym <snizhko@gmail.com>
2025-03-28 15:34:26 +0200

    first task v0.2 HW10

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -10,10 +10,12 @@ import 'izitoast/dist/css/iziToast.min.css';
 const selector = document.querySelector('#datetime-picker');
 const btnStart = document.querySelector('[data-start]');
 const labels = [...document.querySelectorAll('.label')];
+const valueTime = [...document.querySelectorAll(".value")];
 
 labels.forEach(item => (item.textContent = item.textContent.toUpperCase()));
 
 let userSelectedDate;
+let dateNow;
 
 btnStart.disabled = true;
 
@@ -23,19 +25,19 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
-    const dateNow = new Date();
+    //console.log(selectedDates[0]);
+    dateNow = new Date();
     userSelectedDate = selectedDates[0];
     if (userSelectedDate < dateNow) {
       //window.alert('Please choose a date in the future');
       iziToast.show({
-        icon: 'x',
         color: 'red',
         position: 'topRight',
         message: 'Please choose a date in the future',
       });
     } else {
       btnStart.disabled = false;
+      valueTime.forEach((item,index) => valueTime[index].textContent = "00");
     }
   },
 };
@@ -46,4 +48,28 @@ btnStart.addEventListener('click', onClickStart);
 
 function onClickStart(event) {
   console.log(btnStart);
+  btnStart.disabled = true;
+
+  const difference = convertMs(userSelectedDate - dateNow);
+  Object.values(difference).forEach((value,index) => valueTime[index].textContent = value
+  )
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
 }
