779d930f3c54ae560136d7cc9aa8d0668fe8871b
vadim <snizhko@gmail.com>
2025-03-30 01:54:37 +0200

    first task HW 10

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -10,7 +10,7 @@ import 'izitoast/dist/css/iziToast.min.css';
 const selector = document.querySelector('#datetime-picker');
 const btnStart = document.querySelector('[data-start]');
 const labels = [...document.querySelectorAll('.label')];
-const valueTime = [...document.querySelectorAll(".value")];
+const valueTime = [...document.querySelectorAll('.value')];
 
 labels.forEach(item => (item.textContent = item.textContent.toUpperCase()));
 
@@ -26,8 +26,8 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     //console.log(selectedDates[0]);
-    dateNow = new Date();
     userSelectedDate = selectedDates[0];
+    dateNow = new Date();
     if (userSelectedDate < dateNow) {
       //window.alert('Please choose a date in the future');
       iziToast.show({
@@ -37,7 +37,7 @@ const options = {
       });
     } else {
       btnStart.disabled = false;
-      valueTime.forEach((item,index) => valueTime[index].textContent = "00");
+      valueTime.forEach((item, index) => (valueTime[index].textContent = '00'));
     }
   },
 };
@@ -47,29 +47,53 @@ flatpickr('#datetime-picker', options);
 btnStart.addEventListener('click', onClickStart);
 
 function onClickStart(event) {
-  console.log(btnStart);
   btnStart.disabled = true;
+  selector.disabled = true;
+  let diffDate = userSelectedDate - dateNow;
+  let difference = convertMs(diffDate);
 
-  const difference = convertMs(userSelectedDate - dateNow);
-  Object.values(difference).forEach((value,index) => valueTime[index].textContent = value
-  )
+  if (diffDate > 0) {
+    const timerId = setInterval(() => {
+      countDown(diffDate);
+      diffDate -= 1000; // Коректно обчислюємо залишок часу
+
+      if (diffDate <= 0) {
+        valueTime.forEach(item => (item.textContent = '00')); // Обнуляємо таймер
+        selector.disabled = false;
+        clearInterval(timerId); // Зупиняємо таймер, коли час вийшов
+        return;
+      }
+    }, 1000);
+  }
+}
+
+function countDown(value) {
+  const minusSecond = convertMs(value);
+  Object.values(minusSecond).forEach(
+    (value, index) => (valueTime[index].textContent = value)
+  );
 }
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
+
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
   // Remaining days
-  const days = Math.floor(ms / day);
+  const days = pad(Math.floor(ms / day));
   // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
+  const hours = pad(Math.floor((ms % day) / hour));
   // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
   // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
+}
+
+function pad(value) {
+  return String(value).padStart(2, '0');
 }
